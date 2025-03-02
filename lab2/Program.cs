4e01afd8b9035f7381cfce0914e825d1a32601d7
MaxNoragami <maxcraftmd@gmail.com>
2025-03-03 00:16:36 +0200

    A bit of clean up

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -7,13 +7,21 @@ namespace lab2
         static void Main()
         {
             // Variant 1
+            // Alexei Maxim, FAF-232
+            
+            // Task 2.
+            // Possible States
             HashSet<HashSet<string>> q = new HashSet<HashSet<string>>() {
                 new HashSet<string>(){"q0"}, 
                 new HashSet<string>(){"q1"}, 
                 new HashSet<string>(){"q2"}, 
                 new HashSet<string>(){"q3"}
             };
+
+            // Possible transition functions
             HashSet<char> sigma = new HashSet<char>() {'0', '1'};
+
+            // The available transitions
             Dictionary<(HashSet<string>, char), HashSet<string>> delta = new Dictionary<(HashSet<string>, char), HashSet<string>> {
                 { (q.ElementAt(0), '0'), new HashSet<string>() {"q0", "q2"} },
                 { (q.ElementAt(0), '1'), new HashSet<string>() {"q0", "q1"} },
@@ -22,39 +30,51 @@ namespace lab2
                 { (q.ElementAt(3), '0'), new HashSet<string>() {"q3"} },
                 { (q.ElementAt(3), '1'), new HashSet<string>() {"q3"} },
             };
+
+            // Final states
             HashSet<HashSet<string>> qF = new HashSet<HashSet<string>>() {new HashSet<string>(){"q3"}};
+            // Initial state
             string q0 = "q0";
 
             // Initializing the FA
             FiniteAutomaton finiteAutomaton = new FiniteAutomaton(q, sigma, delta, q0, qF);
-            
+           
+            // Vizualizing the FA
             Console.WriteLine(finiteAutomaton.ToString());
 
             // Creating a grammar instance from the FA
             Grammar grammar = finiteAutomaton.ToRegularGrammar();
             Console.WriteLine(grammar.ToString());
 
+            // Checking if the FA is deterministic
             Console.WriteLine(finiteAutomaton.IsDFA());
 
+            // We convert the NFA to DFA
             FiniteAutomaton dfa = finiteAutomaton.ToDFA();
             Console.WriteLine(dfa.ToString());
 
+            // We create the grammar of the DFA that was created out of the NFA
             Grammar dfaGrammar = dfa.ToRegularGrammar();
             Console.WriteLine(dfaGrammar.ToString());
 
+
             Console.WriteLine(dfa.IsDFA());
             
-            Console.WriteLine(CheckGrammars());
+            // Task 1. Checking different grammar types, via input from keyboard
+            CheckGrammars();
 
         }
 
+        // Helper function for Task 1, dealing with keyboard input
         static int CheckGrammars()
         {
+            // Setting the variables in order to create a grammar instance
             string start = string.Empty;
             HashSet<HashSet<string>> nonTerminals = new HashSet<HashSet<string>>();
             HashSet<char> terminals = new HashSet<char>();
             List<(string, HashSet<string>)> rules = new List<(string, HashSet<string>)>();
 
+            // Entering the Non-Terminal values
             Console.WriteLine("\n--- Instantiate Your Grammar for Type Checking---");
             Console.Write("\nNon-Terminals: ");
             string? nonTerminalsInput = Console.ReadLine();
@@ -67,6 +87,7 @@ namespace lab2
             string[] nonTerminalsSplit = nonTerminalsInput.Split(',');
             foreach(var variable in nonTerminalsSplit) nonTerminals.Add(new HashSet<string>(){variable});
             
+            // Entering the Terminal values
             Console.Write("\nTerminals: ");
             string? terminalsInput = Console.ReadLine();
             if(terminalsInput == null)
@@ -79,7 +100,8 @@ namespace lab2
             terminalsInput = terminalsInput.Replace(",", "");
             char[] terminalsSplit = terminalsInput.ToCharArray();
             foreach(var variable in terminalsSplit) terminals.Add(variable);
-
+            
+            // Entering the Start value
             Console.Write("\nStart: ");      
             string? startInput = Console.ReadLine();
             if(startInput == null)
@@ -90,12 +112,14 @@ namespace lab2
             start = startInput.ToUpper();
             if(!nonTerminals.Any(v => v.Contains(start))) nonTerminals.Add(new HashSet<string>(){start});
 
+            // Entering prouduction rules
             int ruleCounter = 0;
             Console.Write("INFO:\n\t- '|' to show more possiblities for the RHS\n\t- 'cancel' to restart the addition of rules\n");
             while(true)
             {
                 ruleCounter += 1;
 
+                // Entering the Left Hand Side of the rule
                 Console.WriteLine("\n--- Enter the Prodution Rule #{0} ---", ruleCounter);
                 Console.Write("LHS: ");
                 string? lhsRawInput = Console.ReadLine();
@@ -103,6 +127,7 @@ namespace lab2
                 lhsRawInput = lhsRawInput.Replace(" ", "");
                 if(lhsRawInput == "cancel") continue;
 
+                // Entering the Right Hand Side of the rule
                 Console.Write("\nRHS: ");
                 string? rhsRawInput = Console.ReadLine();
                 if(rhsRawInput == null) continue;
@@ -110,14 +135,17 @@ namespace lab2
                 if(rhsRawInput == "cancel") continue;
                 string[] rhsInput = rhsRawInput.Split('|');
                 HashSet<string> rhsRule = rhsInput.ToHashSet();
-                // foreach(string rhsRule in rhsInput) rules.Add((lhsRawInput, rhsRule));
+
+                // Adding those to the set of rules
                 rules.Add((lhsRawInput, rhsRule));
                 Console.WriteLine("\nAre you adding more rules? [Anything/no]?");
                 Console.Write("Answer: ");
                 string? moreRulesToBeAdded = Console.ReadLine();
                 if(moreRulesToBeAdded == null || moreRulesToBeAdded.ToLower() == "n" || moreRulesToBeAdded.ToLower() == "no") break;
             }
-        
+
+            // A little bit of automation, if we have a type 3 grammar 
+            // then we construct a Finite Automaton that coresponds to it
             Grammar createdGrammar = new Grammar(nonTerminals, terminals, start, rules);
             int type = createdGrammar.CheckType();
             if(type == 3)
@@ -126,15 +154,9 @@ namespace lab2
                 Console.WriteLine(createdGrammar.ToString());
                 FiniteAutomaton finiteAutomaton = createdGrammar.ToFiniteAutomaton();
                 Console.WriteLine(finiteAutomaton.ToString());
-                // if(!finiteAutomaton.IsDFA())
-                // {
-                //     FiniteAutomaton dfaFiniteAutomaton = finiteAutomaton.ToDFA();
-                //     Console.WriteLine(dfaFiniteAutomaton.ToString());
-                //     Grammar dfaGrammar = dfaFiniteAutomaton.ToRegularGrammar();
-                //     Console.WriteLine(dfaGrammar.ToString());
-                // }
             }
 
+            // Returning the Type
             return type;
         }
     }
