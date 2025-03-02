63fbb8728dbeb10f95b5b4f914d8d1cdf0981528
MaxNoragami <maxcraftmd@gmail.com>
2025-03-03 01:06:33 +0200

    Feat: generate graph based on FA

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -19,26 +19,25 @@ namespace lab2
             };
 
             // Possible transition functions
-            HashSet<char> sigma = new HashSet<char>() {'0', '1'};
+            HashSet<char> sigma = new HashSet<char>() {'a', 'b', 'c'};
 
             // The available transitions
             Dictionary<(HashSet<string>, char), HashSet<string>> delta = new Dictionary<(HashSet<string>, char), HashSet<string>> {
-                { (q.ElementAt(0), '0'), new HashSet<string>() {"q0", "q2"} },
-                { (q.ElementAt(0), '1'), new HashSet<string>() {"q0", "q1"} },
-                { (q.ElementAt(1), '1'), new HashSet<string>() {"q3"} },
-                { (q.ElementAt(2), '0'), new HashSet<string>() {"q3"} },
-                { (q.ElementAt(3), '0'), new HashSet<string>() {"q3"} },
-                { (q.ElementAt(3), '1'), new HashSet<string>() {"q3"} },
+                { (q.ElementAt(0), 'a'), new HashSet<string>() {"q0", "q1"} },
+                { (q.ElementAt(1), 'c'), new HashSet<string>() {"q1"} },
+                { (q.ElementAt(1), 'b'), new HashSet<string>() {"q2"} },
+                { (q.ElementAt(2), 'b'), new HashSet<string>() {"q3"} },
+                { (q.ElementAt(3), 'a'), new HashSet<string>() {"q1"} },
             };
 
             // Final states
-            HashSet<HashSet<string>> qF = new HashSet<HashSet<string>>() {new HashSet<string>(){"q3"}};
+            HashSet<HashSet<string>> qF = new HashSet<HashSet<string>>() {new HashSet<string>(){"q2"}};
             // Initial state
             string q0 = "q0";
 
             // Initializing the FA
             FiniteAutomaton finiteAutomaton = new FiniteAutomaton(q, sigma, delta, q0, qF);
-           
+
             // Vizualizing the FA
             Console.WriteLine(finiteAutomaton.ToString());
 
@@ -53,6 +52,9 @@ namespace lab2
             FiniteAutomaton dfa = finiteAutomaton.ToDFA();
             Console.WriteLine(dfa.ToString());
 
+            // Generating the Graph
+            dfa.GenerateGraph();
+            
             // We create the grammar of the DFA that was created out of the NFA
             Grammar dfaGrammar = dfa.ToRegularGrammar();
             Console.WriteLine(dfaGrammar.ToString());
