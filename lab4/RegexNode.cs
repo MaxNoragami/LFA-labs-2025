bab37c44593de027a70a67b7148a7f900640972e
MaxNoragami <maxcraftmd@gmail.com>
2025-03-24 01:01:04 +0200

    Implemented the 'RegexNode' class, that basically represents the template of a node/token def

---
+++ lab4/RegexNode.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class RegexNode
+    {
+        public RegexNodeType Type { get; set; }
+        public string Value { get; set; } = "";
+        public List<RegexNode> Children { get; set; } = new List<RegexNode>();
+        public int MinRepeat { get; set; }
+        public int MaxRepeat { get; set; }
+        
+        public RegexNode(RegexNodeType type)
+        {
+            Type = type;
+        }
+    }
+}
