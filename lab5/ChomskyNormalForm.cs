2ea31cc8ecada839888617de9afc2d4d67e1e870
MaxNoragami <maxcraftmd@gmail.com>
2025-04-13 18:15:43 +0300

    feat: set up the class that will bring the CFG to CNF

---
+++ lab5/ChomskyNormalForm.cs
@@ -0,0 +1,37 @@
+using System.Text;
+
+namespace lab5
+{
+    public static class ChomskyNormalForm
+    {
+        private static string GetNextAvailableLetter(Grammar grammar)
+        {
+            
+        }
+
+        private static void EliminateEmptyProductions(Grammar grammar)
+        {
+            
+        }
+
+        private static void EliminateAnyUnitRules(Grammar grammar)
+        {
+            
+        }
+
+        private static void EliminateInaccessibleSymbols(Grammar grammar)
+        {
+            
+        }
+
+        private static void EliminateNonProdcutiveSymbols(Grammar grammar)
+        {
+            
+        }
+
+        public static void Obtain(Grammar grammar)
+        {
+            
+        }
+    }
+}
