35c01c1ff8275337746a24742d8a149dde9c3108
MaxNoragami <maxcraftmd@gmail.com>
2025-04-21 20:29:05 +0300

    chore: copy the lexer implementation from 'lab3' in order to implement the parser based on it

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using lab6.Lexer;
+
+var testInputDir = Directory.EnumerateFiles("TestInputs");
+
+foreach(var testInputFile in testInputDir)
+{
+    if(testInputFile.Contains(".pixil"))
+    {
+        string input = File.ReadAllText(testInputFile);
+        Console.WriteLine("\n\n{0}",input);
+
+        // Tokenization Process
+        Tokenizer lexer = new Tokenizer(input);
+        List<Token> tokens = lexer.Tokenize();
+
+        Console.WriteLine("\nTokens, detailed view:");
+        foreach (var token in tokens)
+            Console.WriteLine($"{token.Type}: '{token.Value}' at line {token.Line}, column {token.Column}");
+
+    }
+}
